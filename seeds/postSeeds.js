17a58ae73d584847f6b175ed740ac6953820c78d
mintyry <gitmintyry@gmail.com>
2023-11-25 14:43:47 -0800

    adjusted margins and some styles to single post html, changed post seeds' content

--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -4,25 +4,25 @@ const postData =
 [
   {
     "title": "It's Thanksgiving",
-    "content": "I am coding on Thanksgiving, lol.",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "date_created": "1/1/2023",
     "user_id": 1
   },
   {
     "title": "This is a seeded post",
-    "content": "This wasn't dynamically made by anyone!",
+    "content": "Et consequatur nobis in dolorum recusandae est omnis laudantium. Non quia nulla et illo perspiciatis et voluptas quia id aperiam ratione non maiores autem. ",
     "date_created": "1/2/2023",
     "user_id": 2
   },
   {
     "title": "Inception is related to The Matrix",
-    "content": "This is all a dream.",
+    "content": "There is a bronze marker placed by the Multnomah Chapter of the Daughters of the American Revolution on May 12, 1939.",
     "date_created": "1/3/2023",
     "user_id": 3
   },
   {
     "title": "Second post by user 1",
-    "content": "Testing to see if this fourth post is seeded to user 1/ryan",
+    "content": "It was a rat's nest. Not a literal one, but that is what her hair seemed to resemble every morning when she got up. It was going to take at least an hour to get it under control and she was sick and tired of it.",
     "date_created": "1/7/2023",
     "user_id": 1
   }
