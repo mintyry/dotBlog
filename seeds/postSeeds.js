a4430abc74e1c7cb15fe43119a69e9f3796567f4
mintyry <gitmintyry@gmail.com>
2023-11-25 21:06:27 -0800

    updated post seeds

--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -19,12 +19,6 @@ const postData =
     "content": "There is a bronze marker placed by the Multnomah Chapter of the Daughters of the American Revolution on May 12, 1939.",
     "date_created": "1/3/2023",
     "user_id": 3
-  },
-  {
-    "title": "Second post by user 1",
-    "content": "It was a rat's nest. Not a literal one, but that is what her hair seemed to resemble every morning when she got up. It was going to take at least an hour to get it under control and she was sick and tired of it.",
-    "date_created": "1/7/2023",
-    "user_id": 1
   }
 ];
 
